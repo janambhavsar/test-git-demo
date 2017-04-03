3ccfe275b3f05fabca5d315de42716e697186b1b
Janam Bhavsar <janamb@cybage.com>
2017-04-03 15:17:03 +0530

    tested new file

---
+++ handson_01.js
@@ -0,0 +1,16 @@
+console.log('Example of running a file in nodejs.');
+console.log('============');
+console.log("Hello World!!");
+
+var add = function(a, b) {
+	console.log("Adding");
+	console.log(a + b);
+}
+
+var multiply = function(a, b) {
+	console.log("Multiplying");
+	console.log(a * b);
+}
+
+add(2, 3);
+multiply(2, 3);
